dd3f9f6813c3177c27998b63ad91466c1de58601
Irinaroll <114502352+Irinaroll@users.noreply.github.com>
2022-10-27 23:23:00 +0300

    https://github.com/Irinaroll/task2

--- js/about_us-carousel.js
+++ js/about_us-carousel.js
@@ -1,11 +1,11 @@
 (function () {
     const slides = [
-        '<div class="carousel-about"><img class="svg__about-us" width="50" src="img/soil_for_plants.svg" alt="Soil for Plants"></div>',
-        '<div class="carousel-about"><img class="svg__about-us" width="50" src="img/for_home.svg" alt="Plants for Home"></div>',
-        '<div class="carousel-about"><img class="svg__about-us" width="50" src="img/consultations.svg" alt="Consultations"></div>',
-        '<div class="carousel-about"><img class="svg__about-us" width="50" src="img/flowerpots.svg" alt="Sale of Flowerpots"></div>',
-        '<div class="carousel-about"><img class="svg__about-us" width="50" src="img/plant_food.svg" alt="Plant Food"></div>',
-        '<div class="carousel-about"><img class="svg__about-us" width="50" src="img/garden.svg" alt="Plants for Garden"></div>'
+        '<div class="slide"><img src="img/soil_for_plants.svg" alt="Soil for Plants"></div>',
+        '<div class="slide"><img src="img/for_home.svg" alt="Plants for Home"></div>',
+        '<div class="slide"><img src="img/consultations.svg" alt="Consultations"></div>',
+        '<div class="slide"><img src="img/flowerpots.svg" alt="Sale of Flowerpots"></div>',
+        '<div class="slide"><img src="img/plant_food.svg" alt="Plant Food"></div>',
+        '<div class="slide"><img src="img/garden.svg" alt="Plants for Garden"></div>'
     ];
     let currentSlideIdx = 0;
     function renderSlide() {
