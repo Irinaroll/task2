528df46bfae3ed36e6d1cfeb6e78f5d9f11c4939
Irinaroll <114502352+Irinaroll@users.noreply.github.com>
2022-10-27 22:08:54 +0300

    https://github.com/Irinaroll/task2

--- about_us-carousel.js
+++ about_us-carousel.js
@@ -1,5 +1,22 @@
 (function () {
     const slides = [
-        <div class="carousel-about"></div>
-    ]
-})()
+        '<div class="carousel-about"><img class="svg__about-us" width="50" src="img/soil_for_plants.svg" alt="Soil for Plants"></div>',
+        '<div class="carousel-about"><img class="svg__about-us" width="50" src="img/for_home.svg" alt="Plants for Home"></div>',
+        '<div class="carousel-about"><img class="svg__about-us" width="50" src="img/consultations.svg" alt="Consultations"></div>',
+        '<div class="carousel-about"><img class="svg__about-us" width="50" src="img/flowerpots.svg" alt="Sale of Flowerpots"></div>',
+        '<div class="carousel-about"><img class="svg__about-us" width="50" src="img/plant_food.svg" alt="Plant Food"></div>',
+        '<div class="carousel-about"><img class="svg__about-us" width="50" src="img/garden.svg" alt="Plants for Garden"></div>'
+    ];
+    let currentSlideIndx = 0;
+    function renderSlide() {
+        const slideContainer = document.querySelector('.about-us__button-js');
+        slideContainer.innerHTML - slides[currentSlideIndx];
+    }
+    function nextSlide() {
+        currentSlideIndx = currentSlideIndx + 1 > slides.length ? 0 : currentSlideIndx + 1;
+        renderSlide();
+    }
+    setInterval(nextSlide, 1000);
+
+})();
+
