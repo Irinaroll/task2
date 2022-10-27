17904fd667bc20bbd1f8d73451eb8d7bc0be20ce
Irinaroll <114502352+Irinaroll@users.noreply.github.com>
2022-10-28 01:06:58 +0300

    https://github.com/Irinaroll/task2

--- js/about_us-carousel.js
+++ js/about_us-carousel.js
@@ -1,75 +1,41 @@
-(function () {
+(function() {
+
     const slides = [
-        '<div class="slide"><img src="img/soil_for_plants.svg" alt="Soil for Plants"></div>',
-        '<div class="slide"><img src="img/for_home.svg" alt="Plants for Home"></div>',
-        '<div class="slide"><img src="img/consultations.svg" alt="Consultations"></div>',
-        '<div class="slide"><img src="img/flowerpots.svg" alt="Sale of Flowerpots"></div>',
-        '<div class="slide"><img src="img/plant_food.svg" alt="Plant Food"></div>',
-        '<div class="slide"><img src="img/garden.svg" alt="Plants for Garden"></div>'
+       '<div class="about-us__carousel__item"><img src="img/consultations.png" alt=" " class="about-us__carousel__png"></div>',
+       '<div class="about-us__carousel__item"><img src="img/flowerpots.png" alt=" " class="about-us__carousel__png"></div>',
+       '<div class="about-us__carousel__item"><img src="img/for-home.png" alt=" " class="about-us__carousel__png"></div>',
+       '<div class="about-us__carousel__item"><img src="img/garden.png" alt=" " class="about-us__carousel__png"></div>',
+       '<div class="about-us__carousel__item"><img src="img/plant_food.png" alt=" " class="about-us__carousel__png"></div>'
     ];
-    let currentSlideIdx = 0;
+    let currentSlideIndx = 0;
     function renderSlide() {
-        const slideContainer = document.querySelector('.about-us__button-js');
-        slideContainer.innerHTML - slides[currentSlideIdx];
+       const slideContainer = document.querySelector('.about-us__carousel-container');
+       slideContainer.innerHTML = slides[currentSlideIndx];
+       if (window.innerWidth > 768) {
+          const secondSlideInd = currentSlideIndx + 1 >= slides.length ? 0 : currentSlideIndx + 1;
+          slideContainer.innerHTML += slides[secondSlideInd];
+          if (window.innerWidth > 992) {
+             const thirdSlideInd = secondSlideInd + 1 >= slides.length ? 0 : secondSlideInd + 1;
+             const fourthSlideInd = thirdSlideInd + 1 >= slides.length ? 0 : thirdSlideInd + 1;
+          slideContainer.innerHTML += slides[thirdSlideInd] +  slides[fourthSlideInd];
+          }
+       }
     }
     function nextSlide() {
-        currentSlideIdx = currentSlideIdx + 1 >= slides.length ? 0 : currentSlideIdx + 1;
-        renderSlide();
+       currentSlideIndx = currentSlideIndx + 1 >= slides.length ? 0 : currentSlideIndx + 1;
+       renderSlide();
     }
-    setInterval(nextSlide, 1000);
-
-})();
-$(document).ready(function(){
-    let sliderPosition = 0; // начальная позиция дорожки
-    const sliderContainer = $('.slider-container');
-    const sliderTrack = $('.slider-track');
-    const sliderItem = $('.slider-item');
-    const sliderItemWidth = sliderItem.width();
-    const sliderContainerWidth = sliderContainer.width();
-    // ширина дорожки определяется как разница между шириной всех картинок и шириной контейнера
-    // разница нужна для того, чтобы прокрутка не проводилась дальше последнего фото
-    const sliderTrackWidth = sliderItem.length * sliderItemWidth - sliderContainerWidth; 
-    const sliderButtonPrev = $('.arrow-left');
-    const sliderButtonNext = $('.arrow-right');
-
-    sliderButtonPrev.on('click', function(){
-        sliderPosition += sliderItemWidth; // увеличиваем отступ при нажатии назад
-
-        // поскольку отступ будет всегда отрицательный, нужно сравнивать с нулем, 
-        // чтобы исключить пустые прокрутки
-        if (sliderPosition > 0) {
-            sliderPosition = 0;
-        }
-        sliderTrack.css('transform', `translateX(${sliderPosition}px`);
-        sliderButtons();
-    });
-
-    sliderButtonNext.on('click', function(){
-        sliderPosition -= sliderItemWidth;
-
-        // так как отступы отрицательные, нужно сравнить с отрицательной длинной дорожки, 
-        // чтобы исключить пустые прокрутки
-        if (sliderPosition < -sliderTrackWidth) {
-            sliderPosition = -sliderTrackWidth;
-        }
-        sliderTrack.css('transform', `translateX(${sliderPosition}px`);
-        sliderButtons();
-    });
-
-
-    // скрываем кнопки prev/next, когда нельзя больше крутить
-    const sliderButtons = () => {
-        if (sliderPosition == 0) {
-            sliderButtonPrev.hide();
-        } else {
-            sliderButtonPrev.show();
-        }
-        if (sliderPosition == -sliderTrackWidth) {
-            sliderButtonNext.hide();
-        } else {
-            sliderButtonNext.show();
-        }
-    };
-    sliderButtons();
-});
-
+    function prevSlide() {
+       currentSlideIndx = currentSlideIndx - 1 < 0 ? slides.length - 1 : currentSlideIndx - 1;
+       renderSlide();
+    }
+    setInterval(nextSlide, 3000);
+    renderSlide();
+    const nextBtn = document.querySelector('.about-us__carousel-next');
+    nextBtn.addEventListener('click', nextSlide);
+ 
+    const prevBtn = document.querySelector('.about-us__carousel-prev');
+    prevBtn.addEventListener('click', prevSlide);
+ 
+    window.addEventListener('resize', renderSlide)
+ })();
